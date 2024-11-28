5002175b7d9a5c4cee4ee1a8ea88c02bb9cd9a2e
Denis Bunak <xisi926@ukr.net>
2024-11-28 17:50:23 +0200

    Добавлена страница входа/регистрации юзера.

--- js/main.js
+++ js/main.js
@@ -18,6 +18,14 @@ document.querySelector(".school-name").onmouseout = function () {
   document.querySelector(".al-vadud").style.fill = "red";
 };
 
+// Изменение цвета логотипа незарегистрированого юзера
+document.querySelector("#user").onmouseover = function () {
+  document.querySelector("#user").style.fill = "gold";
+};
+document.querySelector("#user").onmouseout = function () {
+  document.querySelector("#user").style.fill = "#4b4c4d";
+};
+
 //Search box
 const body = (searchToggle = document.querySelector(".searchToggle"));
 
@@ -25,3 +33,19 @@ const body = (searchToggle = document.querySelector(".searchToggle"));
 searchToggle.addEventListener("click", () => {
   searchToggle.classList.toggle("active");
 });
+
+function check() {
+  var rarr = document.getElementsByName("action");
+  const c1 = document.getElementsByClassName("signin-span");
+  const c2 = document.getElementsByClassName("signup-span");
+  const c3 = document.getElementsByClassName("reset-span");
+  if (rarr[0].checked) {
+    c1.style.display = "block";
+  }
+  if (rarr[1].checked) {
+    c2.style.display = "block";
+  }
+  if (rarr[2].checked) {
+    c3.style.display = "block";
+  }
+}
