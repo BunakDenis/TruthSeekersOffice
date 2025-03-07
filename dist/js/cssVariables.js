0522c859d56cda978bf069a8557c0f07db24de15
Denis Bunak <xisi926@ukr.net>
2025-03-07 17:57:48 +0200

    Перенесана иконка Альвадуда в папку icon

--- dist/js/cssVariables.js
+++
@@ -1,1 +0,0 @@
-const rootStyles=getComputedStyle(document.documentElement);export const pageTextColorHover=rootStyles.getPropertyValue("--page-text-color-hover").trim();export const sidebarMenuBackgroundColorHover=rootStyles.getPropertyValue("--cabinet-page-menu-item-hover-background-color").trim();export const alVadudTextColor=rootStyles.getPropertyValue("--al-vadud-text-color").trim();
